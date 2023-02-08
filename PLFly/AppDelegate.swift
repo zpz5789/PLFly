dbfebee91684ba66803d1730c65a470b91626c3d
曾鹏展 <cengpengzhan@cengpengzhandeMacBook-Pro.local>
2023-02-08 11:28:35 +0800

    删除测试代码

--- PLFly/AppDelegate.swift
+++ PLFly/AppDelegate.swift
@@ -16,41 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-
-
-//
-//        Log.trace("Called!!!")
-//        Log.debug("Who is self:", self)
-//        Log.info("some", "jects", "here")
-//
-//        Log.warning("one", "two", "three", separator: " - ")
-        
-        
-        
-        
-        
-        //        Log.error(error, terminator: "😱😱😱\n")
-
-        // Override point for customization after application launch.
-        
-        // 2、将字符串转成大写
-//        var tempHex = "0xffffff".uppercased()
-//        // 检查字符串是否拥有特定前缀
-//        // hasPrefix(prefix: String)
-//        // 检查字符串是否拥有特定后缀。
-//        // hasSuffix(suffix: String)
-//        // 3、判断开头： 0x/#/##
-//        if tempHex.hasPrefix("0X") || tempHex.hasPrefix("##") {
-//            let index = tempHex.index(tempHex.startIndex, offsetBy: 2)
-//            tempHex[tempHex.startIndex..<tempHex.endIndex]
-//            let a = tempHex[tempHex.index(tempHex.startIndex, offsetBy: 2)..<tempHex.endIndex]
-//            tempHex = String(tempHex[tempHex.index(tempHex.startIndex, offsetBy: 2)..<tempHex.endIndex])
-//
-//            String(a)
-//        }
-//
-//        print(tempHex)
-        
         return true
     }
 
--- PLFly/Common/TBTheme.swift
+++
@@ -1,8 +0,0 @@
-//
-//  UIConstants.swift
-//  PLFly
-//
-//  Created by 曾鹏展 on 2023/2/1.
-//  app主题相关字体、颜色
-
-import Foundation
